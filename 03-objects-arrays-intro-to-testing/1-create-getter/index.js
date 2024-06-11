73630057b1727ec4118f70e7e5c297a2813bacb0
Maxim_Kalyadin <kalyadin.maxim@gmail.com>
2024-06-11 20:04:16 +0400

    Все задачи к 3 дз

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,5 +4,18 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
+  const massSplit = path.split('.');
 
+  return function (obj) {
+    let result = {...obj};
+
+    massSplit.forEach(value => {
+      if (result?.hasOwnProperty(value)) {
+        result = result[value];
+      } else {
+        result = undefined;
+      }
+    });
+    return result;
+  };
 }
